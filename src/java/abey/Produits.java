854b6731f86c6ec85de2325edb5880bf63ddfa1f
twane <gervail.antoine@gmail.com>
2013-10-20 16:52:11 +0200

    Modif des pages xhtml et du bean produit (ajout nom, et prix)

--- src/java/abey/Produits.java
+++ src/java/abey/Produits.java
@@ -1,12 +1,15 @@
 package abey;
 
 import java.io.Serializable;
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -21,6 +24,29 @@ public class Produits implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "NOM")
+    private String nom;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRIX")
+    private long prix;
+    
+    /*
+    public Produits() {
+    }
+
+    public Produits(Long id) {
+        this.id = id;
+    }
+
+    public Produits(Long id, String nom, long prix) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+    }*/
 
     public Long getId() {
         return id;
@@ -28,6 +54,21 @@ public class Produits implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+     public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public long getPrix() {
+        return prix;
+    }
+
+    public void setPrix(long prix) {
+        this.prix = prix;
     }
 
     @Override
