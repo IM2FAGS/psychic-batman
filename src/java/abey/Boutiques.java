1120dbbf3081d4b003c33b2913dadeb16696a217
twane <gervail.antoine@gmail.com>
2013-10-20 16:42:00 +0200

    Ajout du bean des boutiques

---
+++ src/java/abey/Boutiques.java
@@ -0,0 +1,58 @@
+package abey;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author gervaila
+ */
+@Entity
+@Table(name = "BOUTIQUES")
+public class Boutiques implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Boutiques)) {
+            return false;
+        }
+        Boutiques other = (Boutiques) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "abey.Boutiques[ id=" + id + " ]";
+    }
+    
+}
