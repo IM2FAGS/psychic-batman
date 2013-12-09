93b39219f04e78a10827e80030639debf5ac3c43
mcdooda <nicolas.dod@gmail.com>
2013-12-09 22:10:37 +0100

    FillUtilisateurs marche correctement pour les tests (mot de passe "pass")

--- src/java/abey/util/FillUtilisateurs.java
+++ src/java/abey/util/FillUtilisateurs.java
@@ -22,27 +22,23 @@ public class FillUtilisateurs implements Serializable {
     //private abey.facades.UtilisateurFacade utilEjbFacade;
 
     public void get_util() {
-        Utilisateur utilisateur = new Utilisateur();
-        utilisateur.setMail("ab" + "@cd.ef");
-        utilisateur.setNom("a");
-        utilisateur.setPass("a");
-        utilisateur.setSalt("salt");
-        Calendar cal = Calendar.getInstance();
-        cal.set(2010, 0, (1) % 28);
-        Date d = cal.getTime();
-        utilisateur.setDateNaissance(d);
-        utilisateurService.create(utilisateur);
         for (int i = 0; i < 40; i++) {
-            utilisateur = new Utilisateur();
-            utilisateur.setMail("ab" + i + "@cd.ef");
-            utilisateur.setNom(i + "abcde");
-            utilisateur.setPass("pass");
-            utilisateur.setSalt("salt");
-            cal = Calendar.getInstance();
-            cal.set(2010, 0, (i + 1) % 28);
-            d = cal.getTime();
-            utilisateur.setDateNaissance(d);
-            utilisateurService.create(utilisateur);
+            try {
+                Utilisateur utilisateur = new Utilisateur();
+                utilisateur.setMail("ab" + i + "@cd.ef");
+                utilisateur.setNom(i + "abcde");
+                String salt = Salt.newSalt();
+                utilisateur.setSalt(salt);
+                String passCrypte = Salt.hashPassword("pass", salt);
+                utilisateur.setPass(passCrypte);
+                Calendar cal = Calendar.getInstance();
+                cal.set(2010, 0, (i + 1) % 28);
+                Date d = cal.getTime();
+                utilisateur.setDateNaissance(d);
+                utilisateurService.create(utilisateur);
+            } catch (Exception e) {
+
+            }
         }
     }
 }
