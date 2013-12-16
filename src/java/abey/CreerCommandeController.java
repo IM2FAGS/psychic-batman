81912a22e4a1c35a118adcfb7d29b3225e773856
Remi <Remi@Pc-Rmi-dev>
2013-12-16 17:00:57 +0100

    Commande partie 1 non testée et non testable

---
+++ src/java/abey/CreerCommandeController.java
@@ -0,0 +1,73 @@
+package abey;
+
+import abey.entities.Achat;
+import abey.entities.Commande;
+import abey.entities.Panier;
+import abey.entities.ProduitPanier;
+import abey.entities.Utilisateur;
+import abey.services.CommandeService;
+import abey.util.JsfUtil;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author disavinr
+ */
+@ManagedBean
+@SessionScoped
+public class CreerCommandeController extends AbstractController {
+
+    @EJB
+    private CommandeService commandeService;
+
+    private Commande current;
+
+    public Commande getSelected() {
+        if (current == null) {
+            current = new Commande();
+        }
+        return current;
+    }
+
+    public String create() {
+        try {
+            Utilisateur curUser = getUtilisateurConnecte();
+            if (curUser != null) {
+                curUser.getCommandes().add(current);
+                current.setAcheteur(curUser);
+                List<ProduitPanier> produits = curUser.getPanier().getProduits();
+                List<Achat> achats = current.getAchats();
+                current.setAchats(new ArrayList<Achat>());
+                for (ProduitPanier p : produits) {
+                    Achat a = new Achat();
+                    a.setCommande(current);
+                    a.setPrixUnitaire(p.getVenteImmediate().getPrix());
+                    a.setQuantite(p.getQuantite());
+                    achats.add(a);
+                }
+                curUser.setPanier(new Panier());
+            } else {
+                JsfUtil.addErrorMessage("ConnexionRequise");
+                return null;
+            }
+
+            commandeService.create(current);
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("CommandeCree"));
+            current = null;
+            return "Create";
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("EchecTransaction"));
+            return null;
+        }
+    }
+
+    public void setCommandeService(CommandeService commandeService) {
+        this.commandeService = commandeService;
+    }
+
+}
