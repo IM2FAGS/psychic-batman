c2430c9384d14ee7c1146be85174b1455f7ab9aa
Anthony <anthony.vernagallo@gmail.com>
2013-12-20 08:45:39 +0100

    Fix de derniere minute

---
+++ peuple_produits.sql
@@ -0,0 +1,117 @@
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (54, 'Boite de chocolat lindt', 'Chocolat', 6);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (57, 'Un beau chorizo', 'Chorizo', 6);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (102, 'Jambon italien délicieux', 'Jambon', 6);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (105, 'Des nombreux kinder buenos', 'Kinder Bueno', 6);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (108, 'Coffret de chocolats lindt', 'Chocolat lindt', 6);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (111, 'Un panier de produits régionaux', 'Produits Régionaux', 6);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (114, 'Une bouteille de vin retrouvée dans le titanic', 'Vin du Titanic', 10);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (117, 'Un champagne de valbert', 'Champagne', 10);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (120, 'Une bouteille de Chivas de 12ans d''age', 'Chivas', 10);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (123, 'Un délicieux coteau du layon.
+Médaillé d''or !', 'Vin layon', 10);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (126, 'Un tout petit habit pour bébé.
+Taille : 6 mois', 'Habit pour bébé', 1);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (129, 'Un blouson pour hommes taille 38', 'Blouson homme', 1);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (132, 'Paire de chaussures nike.
+taille : 42', 'Chaussures Nice', 1);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (135, 'Doudoune Maillet pour femme.
+taille : 32', 'Doudoune femme', 1);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (138, 'Jean les vis pour femme
+taille 40', 'Jean femme 40', 1);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (141, 'Un superbe collier en diamants 45 carrats.', 'Collier diamants', 11);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (144, 'Montre galaxy gear compatible avec les téléphones Samsung.', 'Montre Galaxy Gear', 11);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (147, 'Montre lindberg automatique de collection.', 'Montre Lindberg', 11);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (150, 'Montre lunatic II louis cottier, mouvement automatique. Une montre d''une grande précision.', 'Montre Louis Cottier', 11);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (153, 'Sautoir en argent pour être la plus belle dans les soirée.', 'Sautoir en Argent', 11);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (156, 'Cette magnifique brouette vous aidera dans tous vos travaux de jardinage.', 'Brouette verte', 9);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (159, 'Une brouette qui roule, qui roule..', 'Brouette Boss bleue', 9);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (162, 'Une paire de gants indispensables pour le jardin.', 'Gants de jardin', 9);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (165, 'Ces petits sabots très à la mode garderont vos pieds au sec', 'Sabot', 9);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (168, 'Une tondeuse qui se débrouille toute seule, plus besoin de pousser.', 'Tondeuse de Compétition', 9);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (171, 'La Barbie, un grand classique pour votre fille.', 'Barbie', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (174, 'Un beau circuit de voitures, loopings, virages relevés, tout est possible.', 'Circuit de voitures', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (177, 'La copine de barbie', 'Clawdeen', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (180, 'Furby sera le copain idéal pour votre enfant, il parle, il rit, il chante.', 'Furby', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (183, 'La boite de légo ninjaGO, une boîte faite pour les vrais ninja du lego.', 'Lego ninjaGO', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (186, 'Initiez votre enfant avec cette moto à roulettes, parfait pour l''intérieur.', 'Moto de course', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (189, 'Ce pistolet nerf envoie de petites fléchettes en mousse inoffensives jusqu''à 20m.', 'Pistolet Nerf', 5);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (192, 'Buffet en marbre massif, tout noir avec tout plein de tiroirs et tout.', 'Buffet', 8);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (195, 'Petite chaise verte pour les enfants, pas pour les grand !', 'Chaise verte', 8);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (198, 'Une table et ses deux tabourets du meilleur goût, en imitation champignon.', 'Table et Tabourets champignon', 8);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (201, 'Table contenant un rangement très pratique.', 'Table', 8);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (204, 'Table basse sur laquelle vous pourrez poser vos fleurs ou vos pieds en rentrant du boulot', 'Table basse', 8);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (207, 'Magazine GQ juin 2013', 'GQ', 7);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (210, 'Le journal de mickey de juillet 2013, avec ses blagues, ses bd et son cadeau surprise.', 'Le journal de mickey', 7);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (213, 'Faites le point.', 'Le point', 7);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (216, 'Numéro 15 de science et vie, avec en featuring : Iron Man', 'Science et Vie', 7);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (219, 'Numéro spécial Nelson Mandela, hommage !', 'Time', 7);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (222, '2x Parfum VanderBru qui sent trop bon.', 'Parfum VanderBru', 4);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (225, 'Un panier avec plein de produits lulu, il y en aura pour tout le monde à la maison', 'Lulu panier', 4);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (228, 'Parfum pour homme, le cadeau idéal pour les fêtes.', 'Parfum Laurant Dumon', 4);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (231, 'Comme les films à l''eau de rose, mais dans une petite bouteille.', 'Eau de rose', 4);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (234, '6x Gels douche Cotonage pour être tout propre.', 'Gel douche Cotonage', 4);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (237, 'Petit guide sur les lieux à visiter en islande.', 'Guide Islande', 2);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (240, 'Petit guide sur les lieux à visiter au Japon', 'Guide Japon', 2);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (243, 'Beau sac de voyage Longchampignon.', 'Sac de voyage', 2);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (246, 'Une valise comme une autre.', 'Valise', 2);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (249, 'Ecran Led / 3D / HDMI / GEANT', 'Ecran 24pouces', 3);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (252, 'Des enceintes qui crachent du gros son !', 'Enceintes', 3);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (255, 'Pc sur lequel vous pourrez faire tourner un gros Cluster Glassfish, et plein de bases de données master (pas les Slaves).', 'PC Packard-beau', 3);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (258, 'Tablette android pour faire tout !', 'Tablette android', 3);
+INSERT INTO A.PRODUIT (ID, DESCRIPTION, NOM, CATEGORIE_ID) 
+	VALUES (261, 'Grande télé avec un Monsieur dans une voiture dedans (n''affiche rien d''autre..)', 'Télé', 3);
